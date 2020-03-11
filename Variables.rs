411a3bbdd1d2c0861abc135fd0ce54f4147da192
Jacob An <lightyear.thespaceranger@gmail.com>
2020-03-11 18:34:03 +0900

    variables

---
+++ Variables.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let var1 = 0;
+    let mut var2 = 24;
+    let var3: bool = false;
+    let mut var4 = 'a';
+    println!("var1 = {}, var2 = {}, var3 = {}, var4 = {}", var1, var2, var3, var4);
+
+    let var1 = "String";
+    var2 = 5;
+    let var3 = true;
+    var4 = '👌';
+    println!("var1 = {}, var2 = {}, var3 = {}, var4 = {}", var1, var2, var3, var4);
+}
