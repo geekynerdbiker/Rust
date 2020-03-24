38a9925a712582330f14b15485bddd4fa9512445
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-03-24 20:05:03 +0900

    Pig Latin Converter

--- Custom/pig_latin.rs
+++ Custom/pig_latin.rs
@@ -1,22 +1,30 @@
-fn main() {
-    let s = "Hello";
+use std::io;
 
-    println!("{}", pig_latin(s));
+fn main() {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s)
+        .expect("Input Error.");
+
+    println!("{}", pig_latin(&s));
 }
 
 fn pig_latin(s :&str) -> String{
     let mut pl = String::new();
-    match s.chars().nth(0) {
-        a => {
-            pl.push_str(s);
-            pl.push_str("-hay");
-        },
-        _ => {
-            pl.push_str(&s[1..]);
-            pl.push('-');
-            pl.push(s.chars().nth(0));
-            pl.push_str("ay");
-        },
+    let ch = match s.chars().nth(0) {
+        Some(ch) => ch,
+        None => panic!("ERR"),
+    };
+
+    if ( ch == 'a' ) || ( ch == 'e' ) || ( ch == 'i' ) || ( ch == 'o' ) || ( ch == 'u' ) {
+        pl.push_str(s);
+        pl.push_str("-hay");
+    }
+    else {
+        pl.push_str(&s[1..]);
+        pl.push('-');
+        pl.push(ch);
+        pl.push_str("ay");
     }
 
     pl
