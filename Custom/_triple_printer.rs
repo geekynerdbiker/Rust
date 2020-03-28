ee1d50faeb4cdf8e753647636f69aa5b8a5d0e06
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-03-28 21:27:14 +0900

    triple printer unSolved

---
+++ Custom/_triple_printer.rs
@@ -0,0 +1,18 @@
+fn main() {
+    for i in 1..1000 {
+        for j in 1..1000 {
+            for k in 1..1000 {
+                let mut a = i; let mut b = 1;
+
+                while b < j && a < k {
+                    a *= i;
+                    b += 1;
+                }
+
+                if a == k {
+                    println!("({}, {}, {})", i, j, k);
+                }
+            }
+        }
+    }
+}
