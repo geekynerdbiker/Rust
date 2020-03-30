288356d69d54888c0c12346c781d06b274f2084f
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-03-30 16:41:34 +0900

    define on struct

---
+++ Basic/generic2.rs
@@ -0,0 +1,14 @@
+struct Point1<T> {
+    x: T,
+    y: T,
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let point1 = Point1 { x: 4, y: 7 };
+    let point2 = Point2 { x: 6, y: 5.4 };
+}
