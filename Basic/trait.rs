a63ab32dbe4e9bdcc5b10ea5e97d68b7755a2e72
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-03-31 23:53:00 +0900

    about trait

--- Basic/trait.rs
+++ Basic/trait.rs
@@ -30,9 +30,33 @@ impl Summary for Tweet {
         format!("@{}", self.username)
     }
 }
+// impl Trait syntax
+fn notify1(item: impl Summary) {
+    println!("Nofity1: {}", item.summarize());
+}
+
+// Trait Border syntax
+fn notify2<T: Summary> (item: T) {
+    println!("Notify2: {}", item.summarize());
+}
+
+fn notify3(item1: impl Summary, item2: impl Summary) {
+    println!("[Notify3]\nitem1: {}, item2: {}", item1.summarize(), item2.summarize());
+}
+
+fn notify4<T: Summary> (item1: T, item2: T) {
+    println!("[Notify3]\nitem1: {}, item2: {}", item1.summarize(), item2.summarize());
+}
 
 fn main() {
     let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
+    let article2 = NewsArticle {
         headline: String::from("Penguins win the Stanley Cup Championship!"),
         location: String::from("Pittsburgh, PA, USA"),
         author: String::from("Iceburgh"),
@@ -49,4 +73,10 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    //notify1(article);
+    //notify2(tweet);
+    //notify3(article, tweet);
+    //notify3(article, article2);
+    //notify4(article, article2);
 }
