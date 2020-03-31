b2275e5d7b788d505c462f23ef7bd828d4ee943f
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-03-31 23:17:48 +0900

    about trait

--- Basic/trait.rs
+++ Basic/trait.rs
@@ -1,42 +1,52 @@
 trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 struct NewsArticle {
     headline: String,
     location: String,
     author: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {}, ({})", self.headline, self.author, self.location)
-    }
-}
-
-struct Message {
-    username: String,
     content: String,
 }
 
-impl Summary for Message {
-    fn summarize(&self) -> String {
-        format!("[New Message] {} ({})", self.content, self.username)
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
 fn main() {
     let article = NewsArticle {
-        headline: String::from("The Rust Programming!"),
-        location: String::from("California"),
-        author: String::from("Jacob"),
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
     };
 
-    let message = Message {
-        username: String::from("Jacob An"),
-        content: String::from("I like to study rust-lang."),
+    println!("New article available! {}", article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
     };
 
-    println!("{}", NewsArticle.summarize());
-    println!("{}", Message.summarize());
+    println!("1 new tweet: {}", tweet.summarize());
 }
