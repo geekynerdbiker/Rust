21af38700078ddf41d5f705dc2acf0896610f707
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-04-01 17:01:05 +0900

    how to call, return trait

---
+++ Basic/trait2.rs
@@ -0,0 +1,78 @@
+trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+trait Display {}
+trait Debug {}
+
+struct NewsArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+// impl Trait syntax
+fn notify1(item: impl Summary) {}
+fn notify2(item1: impl Summary + Display) {}
+
+
+// Trait Border syntax
+fn notify3<T: Summary> (item: T) {}
+fn notify4<T: Summary + Display> (item: T) {}
+
+
+fn some_function1<T: Display + Clone, U: Clone + Debug>(t: T, u: U) -> i32 {}
+fn some_function2<T, U>(t: T, u: U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug, {}
+
+fn return_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn main() {
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
